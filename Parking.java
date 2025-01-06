4f9ac6c23abbee0c9dc052fb65f3f65b6e0ff2b8
Ashish Dwivedi <164765035+Ashish-5225@users.noreply.github.com>
2025-01-06 11:19:20 +0530

    Create ParkingSpot.java

---
+++ Parking.java
@@ -0,0 +1,35 @@
+package com.example.parking;
+
+public class ParkingSpot {
+    private int spotNumber;
+    private boolean isOccupied;
+    private String licensePlate;
+
+    public ParkingSpot(int spotNumber) {
+        this.spotNumber = spotNumber;
+        this.isOccupied = false;
+        this.licensePlate = null;
+    }
+
+    public int getSpotNumber() {
+        return spotNumber;
+    }
+
+    public boolean isOccupied() {
+        return isOccupied;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void parkVehicle(String licensePlate) {
+        this.licensePlate = licensePlate;
+        this.isOccupied = true;
+    }
+
+    public void removeVehicle() {
+        this.licensePlate = null;
+        this.isOccupied = false;
+    }
+}
